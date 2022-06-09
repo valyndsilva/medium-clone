fe59b475878925d018ea50d87df3fe3af6976afb
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-06-09 11:07:47 +0100

    fix Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,11 @@ import logoPic from '../public/medium-logo.png';
 function Header({ color = '' }) {
   return (
     <header
-      className={
-        'w-full flex justify-between py-4 px-28  mx-auto ' + (color ?? '')
-      }
+      className={'flex w-full justify-between py-4 px-4 ' + (color ?? '')}
     >
       <div className="flex space-x-5 ">
         <Link href="/">
-          <div className="w-44 cursor-pointer relative">
+          <div className="w-40 h-20 md:w-30  lg:w-44 cursor-pointer relative">
             <Image
               src={logoPic}
               alt="Medium icon"
@@ -20,11 +18,13 @@ function Header({ color = '' }) {
           </div>
         </Link>
       </div>
-      <div className="hidden md:inline-flex items-center space-x-6 text-black text-sm">
-        <div className="cursor-pointer">Our Story</div>
-        <div className="cursor-pointer">Membership</div>
-        <div className="cursor-pointer">Write</div>
-        <div className="hidden sm:inline cursor-pointer">Sign in</div>
+      <div className=" flex items-center space-x-6 text-black text-sm">
+        <div className="hidden md:inline-flex  space-x-4 ">
+          <div className="cursor-pointer">Our Story</div>
+          <div className="cursor-pointer">Membership</div>
+          <div className="cursor-pointer">Write</div>
+        </div>
+        <div className=" sm:inline cursor-pointer ">Sign in</div>
         <div className="bg-black text-white border-black-600 px-4 py-2 rounded-full ">
           Get Started
         </div>
